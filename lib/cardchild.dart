d3e6d36792a915ca3fb31651add3f178a3536d73
saver999 <72280064+saver999@users.noreply.github.com>
2024-03-28 12:21:03 +0100

    Add Slider Widget

--- lib/cardchild.dart
+++ lib/cardchild.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'constants.dart';
 class cardChild extends StatelessWidget {
-  cardChild({ required this.icon, required this.text});
-  final IconData icon;
+  cardChild({  this.icon, required this.text});
+  final IconData ?icon;
   final String text;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
 
-        children: [
+      children: [
 
-          Icon(
-            color: Colors.white,
-            icon,
-            size: 60.0,
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Text(
-            text,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Color(0xFF8D8E98),
-            ),
-          ),
-        ],
-      ),
+        Icon(
+          color: Colors.white,
+          icon,
+          size: 60.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: kLabelTextStyle,
+        ),
+      ],
     );
   }
 }
---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+
+
+const kLabelTextStyle = TextStyle(
+  fontSize : 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const knumberTextStyle =  TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.w900);
