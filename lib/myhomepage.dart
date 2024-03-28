eeec60a9c902019be9245cda1651e56239e38897
saver999 <72280064+saver999@users.noreply.github.com>
2024-03-28 12:24:22 +0100

    Add Slider Widget

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,19 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'mycard.dart';
 import 'cardchild.dart';
+import 'constants.dart';
 
-const inactiveColour = Color(0xFF111328);
-const activeColour = Color(0xFF1D1E33);
-
-Color maleColour = inactiveColour;
-Color femaleColour = inactiveColour;
-enum Gender {
-  male,
-  female
-}
+enum Gender { male, female }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -24,14 +19,10 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
-
 class _MyHomePageState extends State<MyHomePage> {
-
   bool isActiveMale = false;
   bool isActiveFemale = false;
-
+  int height = 180;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,42 +30,51 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(
           widget.title,
           style: TextStyle(
-              fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white
-          ),
+              fontSize: 23, fontWeight: FontWeight.bold, color: Colors.white),
         ),
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: [
-
-                 Expanded(
-                   child:  GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          isActiveFemale = false;
-                          isActiveMale = true;
-                        });
-                      },
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isActiveFemale = false;
+                        isActiveMale = true;
+                      });
+                    },
                     child: Mycard(
-                      colours: isActiveMale ? activeColour : inactiveColour,
-                      cardChild: cardChild(icon: FontAwesomeIcons.mars,text: 'MALE',),
+                      colours: isActiveMale
+                          ? kActiveCardColour
+                          : kInactiveCardColour,
+                      cardChild: cardChild(
+                        icon: FontAwesomeIcons.mars,
+                        text: 'MALE',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
                   child: GestureDetector(
                     onTap: () {
-                     setState(() {
-                       isActiveFemale = true;
-                       isActiveMale = false;
-                     });
+                      setState(() {
+                        isActiveFemale = true;
+                        isActiveMale = false;
+                      });
                     },
                     child: Mycard(
-                      colours: isActiveFemale ? activeColour : inactiveColour,
-                      cardChild: cardChild(icon: FontAwesomeIcons.venus,text: 'FEMALE',),
+                      colours: isActiveFemale
+                          ? kActiveCardColour
+                          : kInactiveCardColour,
+                      cardChild: cardChild(
+                        icon: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+                      ),
                     ),
                   ),
                 ),
@@ -84,7 +84,49 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Mycard(
               colours: Color(0xFF111328),
-
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "HEIGHT",
+                    style: kLabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        '$height',
+                        style: knumberTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbColor: Colors.amber,
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -114,7 +156,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
-
-
